473defe8015a47de04d4d2405ba87b2644c40f03
zilongshi <zilongshi@gmail.com>
2016-11-04 16:26:29 +0800

    Fixed cedet issue for Emacs 25, force delete, ^_*||

--- init.el
+++ init.el
@@ -142,7 +142,21 @@
 ;; (load-file "~/.emacs.d/site-lisp/008-LaTeX.el")
 
 ;;; cedet setting
-(load-file "~/.emacs.d/site-lisp/minimial-cedet-config.el")
+;; (load-file "~/.emacs.d/site-lisp/minimial-cedet-config.el")
 
 ;;; programming language mode settings
 (load-file "~/.emacs.d/site-lisp/012-programming-mode.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (window-number web-mode use-package undo-tree sws-mode solarized-theme smex slime shell-command react-snippets powerline popup markdown-mode lua-mode js2-mode jade-mode elpy company-irony bash-completion))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
